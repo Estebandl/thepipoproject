f5a09a2b7c8f976764f54e2f41863c5b4e8b06fd
PPIPO <46755626+PPIPO@users.noreply.github.com>
2019-01-23 17:48:57 +0100

    Casi acabado consultas
    ^^

--- php/consultWorkers.php
+++ php/consultWorkers.php
@@ -1,162 +1,161 @@
 <?php
     function worker($dni) {
 
+        print('<link rel="stylesheet" type="text/css" media="screen" href="../css/main.css"/>');
+
         include('connect.php');
 
 		if ($conexion) {
 			$db = $conexion->select_db(DB_DATABASE);
 			if ($db) {
-				$consulta = "SELECT * FROM `empleats`";
+				$consulta = "SELECT * FROM `empleats` WHERE dni = " . "'". $conexion->real_escape_string($dni) ."'";
 
 				$resultado = $conexion->query($consulta);
 
-                print('<table id="tableWorkers">');
+                print('<div id="containerWorkers">');
 
-				while ($fila = $resultado->fetch_assoc()) {
+                    print('<table id="tableWorkers">');
 
-                    print('<tr>');
-                        print('<td>Nom:</td>');
-                        print('<td id="name">');
-                        print($fila['nombre']);
-                        print('</td>');
-                    print('</tr>');
-                    /*print('
-                    <tr>
-                        <td>Cognom1:</td>
-                        <td id="fSurname"></td>
-                    </tr>
-                    <tr>
-                        <td>Cognom2:</td>
-                        <td id="sSurname"></td>
-                    </tr>
-                    <tr>
-                        <td>DNI:</td>
-                        <td id="dni"></td>
-                    </tr>
-                    <tr>
-                        <td>Data naixement: dd-mm-aaaa</td>
-                        <td id="bDate"></td>
-                    </tr>
-                    <tr>
-                        <td>Sexe (Home/Dona)</td>
-                        <td id="sex"></td>
-                    </tr>
-                    <tr>
-                        <td>Nacionalitat:</td>
-                        <td id="country"></td>
-                    </tr>
-                    <tr>
-                        <td>Adreça (Via/nom_carrer/núm /pis/porta)</td>
-                        <td id="adress"></td>
-                    </tr>
-                    <tr>
-                        <td>Codi postal: XXXXX</td>
-                        <td id="postal"></td>
-                    </tr>
-                    <tr>
-                        <td>Població</td>
-                        <td id="poblation"></td>
-                    </tr>
-                    <tr>
-                        <td>Telèfon personal</td>
-                        <td id="phoneNum"></td>
-                    </tr>
-                    <tr>
-                        <td>Mail personal</td>
-                        <td id="email"></td>
-                    </tr>
-                    <tr>
-                        <td>Formació:</td>
-                        <td id="work"></td>
-                    </tr>
-                    <tr>
-                        <td>Disminució: (Percentatge)</td>
-                        <td id="dismunuation"></td>
-                    </tr>
-                    <tr>
-                        <td>Compte bancari: IBAN</td>
-                        <td id="iban"></td>
-                    </tr>
-                    <tr>
-                        <td>Número Seguretat Social</td>
-                        <td id="ssc"></td>
-                    </tr>
-                    <tr>
-                        <td>Telèfon contacte avís emergències</td>
-                        <td id="emergTel"></td>
-                    </tr>
-                    <tr>
-                        <td>Nom contacte avís emergències</td>
-                        <td id="emergName"></td>
-                    </tr>
-                    <tr>
-                        <td>Data alta: dd-mm-aaaa</td>
-                        <td id="startDate"></td>
-                    </tr>
-                    <tr>
-                        <td>Data baixa: dd-mm-aaaa</td>
-                        <td id="endDate"></td>
-                    </tr>
-                    <tr>
-                        <td>Venciment contracte: dd-mm-aaaa</td>
-                        <td id="contractEnd"></td>
-                    </tr>
-                    <tr>
-                        <td>Tipus de contracte</td>
-                        <td id="contractType"></td>
-                    </tr>
-                    <tr>
-                        <td>Grup de cotització</td>
-                        <td id="cotitGroup"></td>
-                    </tr>
-                    <tr>
-                        <td>Informació nómina</td>
-                        <td id="nominInf"></td>
-                    </tr>
-                    <tr>
-                        <td>Excedència voluntària data inici:</td>
-                        <td id="startDateEx"></td>
-                    </tr>
-                    <tr>
-                        <td>Telèfon feina</td>
-                        <td id="workTel"></td>
-                    </tr>
-                    <tr>
-                        <td>Mail feina</td>
-                        <td id="workEmail"></td>
-                    </tr>
-                    <tr>
-                        <td>Àrea assignada</td>
-                        <td id="assignArea"></td>
-                    </tr>
-                    <tr>
-                        <td>Responsable</td>
-                        <td id="resp"></td>
-                    </tr>
-                    <tr>
-                        <td>Projecte assignat</td>
-                        <td id="assingProj"></td>
-                    </tr>
-                    <tr>
-                        <td>Lloc físic de treball</td>
-                        <td id="workPlace"></td>
-                    </tr>
-                    <tr>
-                        <td>Horari</td>
-                        <td id="schedule"></td>
-                    </tr>
-                    <tr>
-                        <td>Jornada setmanal</td>
-                        <td id="weekSchedule"></td>
-                    </tr>
-                    <tr>
-                        <td>Funció / Càrrec</td>
-                        <td id="workPosition"></td>
-                    </tr>');*/
-					
-                }
-                
-                print('</table>');
+                    while ($fila = $resultado->fetch_assoc()) {
+
+                        print('<tr>');
+                            print('<td>Nom:</td>');
+                            print('<td id="name">');
+                            print($fila['nombre']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Cognom1:</td>');
+                            print('<td id="fSurname">');
+                                print($fila['cognom1']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Cognom2:</td>');
+                            print('<td id="sSurname">');
+                                print($fila['cognom2']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>DNI:</td>');
+                            print('<td id="dni">');
+                                print($fila['dni']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Data naixement: dd-mm-aaaa</td>');
+                            print('<td id="bDate">');
+                                print($fila['data_naixement']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Sexe (Home/Dona)</td>');
+                            print('<td id="sex">');
+                                print($fila['sexe']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Nacionalitat:</td>');
+                            print('<td id="country">');
+                                print($fila['dni']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Adreça (Via/nom_carrer/núm /pis/porta)</td>');
+                            print('<td id="adress">');
+                                print($fila['adreca']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Codi postal: XXXXX</td>');
+                            print('<td id="postal">');
+                                print($fila['codi_postal']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Població</td>');
+                            print('<td id="poblation">');
+                                print($fila['poblacion']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Formació:</td>');
+                            print('<td id="work">');
+                                print($fila['formacio']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Disminució: (Percentatge)</td>');
+                            print('<td id="dismunuation">');
+                                print($fila['disminucio']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Compte bancari: IBAN</td>');
+                            print('<td id="iban">');
+                                print($fila['compte_bancari']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Número Seguretat Social</td>');
+                            print('<td id="ssc">');
+                                print($fila['n_seguretat_social']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Àrea assignada</td>');
+                            print('<td id="assignArea">');
+                                print($fila['area_assignada']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Responsable</td>');
+                            print('<td id="resp">');
+                                print($fila['responsable']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Projecte assignat</td>');
+                            print('<td id="assingProj">');
+                                print($fila['projecte_assignat']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Lloc físic de treball</td>');
+                            print('<td id="workPlace">');
+                                print($fila['lloc_fisic_treball']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Inici de Horari</td>');
+                            print('<td id="schedule">');
+                                print($fila['horari_inici']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Fí de Horari</td>');
+                            print('<td id="schedule">');
+                                print($fila['horari_final']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Jornada setmanal</td>');
+                            print('<td id="weekSchedule">');
+                                print($fila['jornada_setmanal']);
+                            print('</td>');
+                        print('</tr>');
+                        print('<tr>');
+                            print('<td>Funció / Càrrec</td>');
+                            print('<td id="workPosition">');
+                                print($fila['funcio_carrec']);
+                            print('</td>');
+                        print('</tr>');
+                        
+                    }
+                    
+                    print('</table>');
+
+                print('</div>');
 
                 include('close.php');
 
@@ -173,6 +172,8 @@
 
 		if (isset($dni)) {
             worker($dni);
+            print('<hr/>');
+            print('<button onClick="location.href = "../consultWorkers.html">Torna a Cercar</button>');
 		} else {
 			print("Torna a introduir el DNI.");
         }
