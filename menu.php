685d88fc6fd48747829cfc4cbdda673ee9d3fe81
Estebandl <44871992+Estebandl@users.noreply.github.com>
2019-02-06 17:33:42 +0100

    Cambio extensiones
    06/02

---
+++ menu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SAO Prat</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script src="js/main.js"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+</head>
+	
+	<?php include 'php/webelements/header.php' ?>
+
+<body>
+	
+	<div class="container"><center>
+		
+	</center></div>
+</body>
+
+</html>
---
+++ php/webelements/header.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand">Benvingut/da</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="menu.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="consultWorkers.php">Consulta</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="insertWorkers.php">Insertar</a>
+      </li>
+    </ul>
+  </div>
+</nav>
