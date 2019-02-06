9eca594f05b0cdefa2d777ca32435dc3f92289ea
Jcandalija <43951596+Jcandalija@users.noreply.github.com>
2019-02-06 16:31:26 +0100

    Insertar Empleados finalizado
    SUUUUUU

--- php/insertWorkersData.php
+++ php/insertWorkersData.php
@@ -1,30 +1,37 @@
 <?php
-
 include('connect.php');
-session_start();
 
-$nom = $_SESSION['nom'];
-$dni = $_SESSION['dni'];
-$primerCognom =$_SESSION['primerCognom'];
-$segonCognom = $_SESSION['segonCognom'];
-$dataNaixament = $_SESSION['dataNaixament'];
-$sexe = $_SESSION['sexe'];
-$nacionalitat = $_SESSION['nacionalitat'];
-$adreça = $_SESSION['adreça'];
-$codiPostal = $_SESSION['codiPostal'];
-$poblacio = $_SESSION['poblacio']; 
-$formacio = $_SESSION['formacio'];
-$disminucio = $_SESSION['disminucio'];
-$compteBancari = $_SESSION['compteBancari'];
-$numeroSS = $_SESSION['numeroSeguretatSocial'];
-$area = $_SESSION['area'];
-$responsable = $_SESSION['responsable'];
-$projecte = $_SESSION['projecte'];
-$llocFisicTreball = $_SESSION['llocFisicTreball'];
-$horaInici = $_SESSION['horaInici'];
-$horaFinal = $_SESSION['horaFinal'];
-$jornadaSetmanal = $_SESSION['jornadaSetmanal'];
-$funcioCarrec = $_SESSION['funcioCarrec'];
+// TAULA EMPLEATS
+$nom = $_POST['nom'];
+$dni = $_POST['dni'];
+$primerCognom =$_POST['primerCognom'];
+$segonCognom = $_POST['segonCognom'];
+$dataNaixament = $_POST['dataNaixament'];
+$sexe = $_POST['sexe'];
+$nacionalitat = $_POST['nacionalitat'];
+$adreça = $_POST['adreça'];
+$codiPostal = $_POST['codiPostal'];
+$poblacio = $_POST['poblacio']; 
+$formacio = $_POST['formacio'];
+$disminucio = $_POST['disminucio'];
+$compteBancari = $_POST['compteBancari'];
+$numeroSS = $_POST['numeroSeguretatSocial'];
+$area = $_POST['area'];
+$responsable = $_POST['responsable'];
+$projecte = $_POST['projecte'];
+$llocFisicTreball = $_POST['llocFisicTreball'];
+$horaInici = $_POST['horaInici'];
+$horaFinal = $_POST['horaFinal'];
+$jornadaSetmanal = $_POST['jornadaSetmanal'];
+$funcioCarrec = $_POST['funcioCarrec'];
+
+// TAULA TELEFON
+$telefon = $_POST['telefon'];
+$tipusTelefon = $_POST['tipusTelefon'];
+$nomContacte = $_POST['nomContacte'];
+
+//TAULA EMAIL
+$email = $_POST['email'];
 
 
 $sql = "INSERT INTO empleats 
@@ -32,10 +39,17 @@ VALUES ('$dni', '$nom', '$primerCognom', '$segonCognom', '$dataNaixament' , $sex
         '$poblacio', '$formacio', $disminucio, '$compteBancari', '$numeroSS', '$area', '$responsable', '$projecte', 
         '$llocFisicTreball', $horaInici, $horaFinal, $jornadaSetmanal, '$funcioCarrec')";
 
-if ($conexion->query($sql) === TRUE) {
-    echo "Empleado añadido.";
+$sql2 = "INSERT INTO telefons
+VALUES ('$dni', '$telefon', '$tipusTelefon', '$nomContacte')";
+
+$sql3 = "INSERT INTO mails
+VALUES ('$dni', '$email', null)";
+
+
+if ($conexion->query($sql) == FALSE || $conexion->query($sql2) == FALSE || $conexion->query($sql3) == FALSE ) {
+    echo 'Error ' .$sql . "<br>" . $conexion->error;
 } else {
-    echo "Error: " . $sql . "<br>" . $conexion->error;
+    echo 'Empleado añadido correctamente';
 }
 
 ?>
