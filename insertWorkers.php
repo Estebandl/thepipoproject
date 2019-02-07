5f0d201a256a3da68bc6a36ab75ee81588ba46f9
Estebandl <44871992+Estebandl@users.noreply.github.com>
2019-02-07 17:56:33 +0100

    cambio fuente 07/02

--- insertWorkers.php
+++ insertWorkers.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Formulari d'introduccio d'empleats</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+        <link rel="stylesheet" type="text/css" href="css/main.css">
         <script src="main.js"></script>
 </head>
 
--- php/webelements/header.php
+++ php/webelements/header.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/main.css">
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand">Benvingut/da</a>
