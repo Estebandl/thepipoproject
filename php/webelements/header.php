e3771d0cf0d879afc49c6decc194536c4420e1de
Estebandl <44871992+Estebandl@users.noreply.github.com>
2019-02-13 16:22:53 +0100

    integracion bootstrap

--- php/webelements/header.php
+++ php/webelements/header.php
@@ -1,4 +1,6 @@
 <link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand">Benvingut/da</a>
