1bf4c7c3ce75380ab0ef38b4d5b24334aee5951b
PPIPO <46755626+PPIPO@users.noreply.github.com>
2019-02-20 15:59:46 +0100

    Formulario de consulta
    Consulta completa

--- consultWorkers.php
+++ consultWorkers.php
@@ -5,6 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Consultar Empleats</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="css/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="css/perfect-scrollbar/perfect-scrollbar.css">
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/mainTables.css">
 </head>
 
 <?php
@@ -12,10 +21,8 @@
 ?>
 
 <body>
-    <form role="form" method="post" action="consultWorkers.php" style="display: flex; justify-content: space-between;">
-        <label>Introdueix el DNI de l'empleat desitjat:
-            <input type="text" name="dni" placeholder="DNI"/>
-        </label>
+    <form role="form" method="post" action="consultWorkers.php" style="display: flex; justify-content: space-between; margin-top: 10px;">
+        <label style="margin-top: 8px;">Introdueix el DNI de l'empleat desitjat: <input type="text" name="dni" placeholder="DNI" style="border-radius: 25px; border: none !important;"/></label>
         <input type="submit" name="consult"/>
         <input type="submit" name="add" value="Insertar"/>
     </form>
@@ -29,5 +36,11 @@
             include("php/workers.php");
         }
     ?>
+
+    <script src="js/jquery/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap/popper.js"></script>
+	<script src="js/bootstrap/bootstrap.min.js"></script>
+	<script src="js/select2/select2.min.js"></script>
+	<script src="js/mainTables.js"></script>
 </body>
 </html>
--- php/workersFiltros.php
+++ php/workersFiltros.php
@@ -11,46 +11,54 @@
             $resultado = $conexion->query($consulta);
             
 
-                print('<div id="containerWorkers">');
+                print('<div class="table100 containerWorkers">');
 
-                    print('<table id="tableWorkers">');
+                    print('<table class="tableWorkers">');
 
-                        print('<tr>');
-                            print('<td>DNI</td>');
-                            print('<td>Nom</td>');
-                            print('<td>Cognom1</td>');
-                            print('<td>Cognom2</td>');
-                            print('<td>Data naixement</td>');
-                            print('<td>Actions</td>');
-                        print('</tr>');
+                        print('<thead>');
 
-                        while ($fila = $resultado->fetch_assoc()) {
-                            
-                            print('<tr>');
-                                print('<td id="dni">');
-                                    print($fila['dni']);
-                                print('</td>');    
-                                print('<td id="name">');
-                                    print($fila['nom']);
-                                print('</td>');
-                                print('<td id="fSurname">');
-                                    print($fila['cognom1']);
-                                print('</td>');
-                                print('<td id="sSurname">');
-                                    print($fila['cognom2']);
-                                print('</td>');
-                                print('<td id="bDate">');
-                                    print($fila['data_naixement']);
-                                print('</td>');
-                                print('<td id="actions">');
-                                    print('<i class="fas fa-address-card"></i>');
-                                    print('<i class="fas fa-address-book"></i>');
-                                    print('<i class="fas fa-pen"></i>');
-                                    print('<i class="fas fa-times"></i>');
-                                print('</td>');
+                            print('<tr class="table100-head">');
+                                print('<td class="column1">DNI</td>');
+                                print('<td class="column2">Nom</td>');
+                                print('<td class="column3">Cognom1</td>');
+                                print('<td class="column4">Cognom2</td>');
+                                print('<td class="column5">Data naixement</td>');
+                                print('<td class="column6">Actions</td>');
                             print('</tr>');
-                            
-                        }
+
+                            print('</thead>');
+                            print('<tbody>');
+
+                            while ($fila = $resultado->fetch_assoc()) {
+                                
+                                print('<tr>');
+                                    print('<td id="dni" class="column1">');
+                                        print($fila['dni']);
+                                    print('</td>');    
+                                    print('<td id="name" class="column2">');
+                                        print($fila['nom']);
+                                    print('</td>');
+                                    print('<td id="fSurname" class="column3">');
+                                        print($fila['cognom1']);
+                                    print('</td>');
+                                    print('<td id="sSurname" class="column4">');
+                                        print($fila['cognom2']);
+                                    print('</td>');
+                                    print('<td id="bDate" class="column5">');
+                                        print($fila['data_naixement']);
+                                    print('</td>');
+                                    print('<td id="actions" class="column6">');
+                                        print('<i class="fas fa-address-card icons" style="padding: 5px;"></i>');
+                                        print('<i class="fas fa-address-book icons" style="padding: 5px;"></i>');
+                                        print('<i class="fas fa-pen icons" style="padding: 5px;"></i>');
+                                        print('<i class="fas fa-times icons" style="padding: 5px;"></i>');
+                                    print('</td>');
+                                print('</tr>');
+                                
+                            }
+                        
+                        print('</tbody');
+
                     print('</table>');
 
                 print('</div>');
