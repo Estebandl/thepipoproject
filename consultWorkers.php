6b9c46e36b5f02c7929d8f105c3956d1a06649a0
PPIPO <46755626+PPIPO@users.noreply.github.com>
2019-02-27 15:26:08 +0100

    Icono
    Cambio a el icono de Sao el Prat

--- consultWorkers.php
+++ consultWorkers.php
@@ -6,7 +6,6 @@
     <title>Consultar Empleats</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/animate/animate.css">
