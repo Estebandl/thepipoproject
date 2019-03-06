ca26ba09b473bd0ef2b535c1b8863e098581f138
Estebandl <44871992+Estebandl@users.noreply.github.com>
2019-03-06 14:43:32 +0100

    errores header

--- php/webelements/header.php
+++ php/webelements/header.php
@@ -2,7 +2,10 @@
   session_start(); 
   $usuari = $_SESSION['usuari'];
 ?>
-
+<link rel="icon" type="image/png" href="img/icons/favicon.png"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="css/main.css">
 
 <div id = "header">
