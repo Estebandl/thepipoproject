e6fb3bc5a5a968980911330c0667dbff1b6c66ba
Estebandl <44871992+Estebandl@users.noreply.github.com>
2019-03-06 15:48:03 +0100

    menu

--- php/webelements/header.php
+++ php/webelements/header.php
@@ -1,29 +1,25 @@
-<?php 
-  session_start(); 
-  $usuari = $_SESSION['usuari'];
-?>
 <link rel="icon" type="image/png" href="img/icons/favicon.png"/>
+<link rel="stylesheet" type="text/css" href="css/main.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="css/main.css">
 
-<div id = "header">
-
-    <ul class="nav">
-    <li><img class="imagen" src="img/logo.png" style="height: 20px;">
-    <ul><br>
-          <li><a class="colores" href="menu.php">Home</a></li><br>
-          <li><a class="colores" href="consultWorkers.php">Consulta</a></li>
-        </ul>
-    </li>
-
-      <li><a class="margenes">Benvingut/da, <?php echo $usuari ?></a></li>
-        <div><img class="apagado" src="img/icons/logout.png"/></div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand">Benvingut/da</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="menu.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="consultWorkers.php">Consulta</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="insertWorkers.php">Insertar</a>
+      </li>
     </ul>
   </div>
 </nav>
-
-<?php
-    include('php/connect.php');    
-?>
