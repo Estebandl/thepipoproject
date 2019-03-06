d04cba8317c9d2268dbfdbcd953d90f2dd8269af
Jcandalija <43951596+Jcandalija@users.noreply.github.com>
2019-03-06 15:45:59 +0100

    Inserción empleados completada
    Se pueden introducir empleados correctamente.

--- insertWorkers.php
+++ insertWorkers.php
@@ -1,135 +1,131 @@
-<!DOCTYPE html>
-<html>
-<head>
-        <meta charset="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Formulari d'introduccio d'empleats</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="css/main.css">
-</head>
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+include('php/insertWorkersData.php');
+?>
 
-<body>
 <div class="modal fade" id="insertar" role="dialog">  
     <div class="modal-dialog">
       <div class="modal-content container">
             <div class="tablas">
-                <form role="form" method="post" action="php/insertWorkersData.php">
+                <form role="form" method="post" action="consultWorkers.php">
 
                 <label for="nom">Nom</label><br>
                 <input type="text" name="nom" required>
                 <br><br>
                 
                 <label for="primerCognom">1er Cognom</label><br>
-                <input type="text" name="primerCognom" >
+                <input type="text" name="primerCognom" required>
                 <br><br>
                 
                 <label for="segonCognom">2on Cognom</label><br>
-                <input type="text" name="segonCognom" >
+                <input type="text" name="segonCognom" required> 
                 <br><br>
                 
                 <label for="dni">DNI</label><br>
-                <input type="text" name="dni" >
+                <input type="text" name="dni" required>
                 <br><br>
                 
                 <label for="dataNaixament">Data de naixement (DD/MM/AAAA)</label><br>
-                <input type="text" name="dataNaixament" >
+                <input type="text" name="dataNaixament" required>
                 <br><br>
                 
                 <label for="sexe">Sexe</label><br>
-                <input type="radio" name="sexe" value="1">Home
-                <input type="radio" name="sexe" value="0">Dona
+                <input type="radio" name="sexe" value="1"required>Home
+                <input type="radio" name="sexe" value="0"required>Dona
                 <br><br>
                 
                 <label for="nacionalitat">Nacionalitat</label><br>
-                <input type="text" name="nacionalitat" >
+                <input type="text" name="nacionalitat" required>
                 <br><br>
                 
                 <label for="adreça">Adreça</label><br>
-                <input type="text" name="adreça" >
+                <input type="text" name="adreça" required>
                 <br><br>
 
                 <label for="codiPostal">Codi postal</label><br>
-                <input type="number" name="codiPostal" >
+                <input type="number" name="codiPostal" required>
                 <br><br>
             </div>
 
             <div class="tablas">
                 <label for="email">Correu electrónic :</label><br>
-                <input type="text" name="email" >
+                <input type="text" name="email" required>
                 <br><br>
 
                 <label for="tipusTelefon">Tipus de telèfon</label><br>
-                <input type="radio" name="tipusTelefon" value="1">Fixe
-                <input type="radio" name="tipusTelefon" value="0">Movil
+                <input type="radio" name="tipusTelefon" value="1" required>Fixe
+                <input type="radio" name="tipusTelefon" value="0" required>Movil
                 <br><br>
 
                 <label for="telefonFixe">Telèfon :</label><br>
-                <input type="text" name="telefon" >
+                <input type="number" name="telefon" required>
                 <br><br>
 
                 <label for="nomContacte">Nom del contacte :</label><br>
-                <input type="text" name="nomContacte" >
+                <input type="text" name="nomContacte" required>
                 <br><br>
                 
                 <label for="poblacio">Població</label><br>
-                <input type="text" name="poblacio" >
+                <input type="text" name="poblacio" required>
                 <br><br>
                 
                 <label for="formacio">Formació</label><br>
-                <input type="text" name="formacio" >
+                <input type="text" name="formacio" required>
                 <br><br>
                 
                 <label for="disminucio">Disminució (%)</label><br>
-                <input type="number" name="disminucio" >
+                <input type="number" name="disminucio" required>
                 <br><br>
 
                 <label for="compteBancari">Compte bancari</label><br>
-                <input type="text" name="compteBancari" >
+                <input type="text" name="compteBancari" required>
                 <br><br>
 
                 <label for="numeroSeguretatSocial">Numero de la Seguretat Social</label><br>
-                <input type="text" name="numeroSeguretatSocial" >
+                <input type="text" name="numeroSeguretatSocial" required>
                 <br><br>
             </div>
 
             <div class="tablas2">
                 <label for="area">Area asignada</label><br>
-                <input type="text" name="area" >
+                <input type="text" name="area" required>
                 <br><br>
                 
                 <label for="responsable">Responsable</label><br>
-                <input type="text" name="responsable" >
+                <input type="text" name="responsable" required>
                 <br><br>
                 
                 <label for="projecte">Projecte assignat</label><br>
-                <input type="text" name="projecte" >
+                <input type="text" name="projecte" required>
                 <br><br>
                 
                 <label for="llcoFisicTreball">Lloc fisic del treball</label><br>
-                <input type="text" name="llocFisicTreball" >
+                <input type="text" name="llocFisicTreball" required>
                 <br><br>
                 
                 <label for="horaInici">Hora d'inici</label><br>
-                <input type="number" name="horaInici" >
+                <input type="number" name="horaInici" required>
                 <br><br>
                 
                 <label for="horaFinal">Hora de finalització</label><br>
-                <input type="number" name="horaFinal" >
+                <input type="number" name="horaFinal" required>
                 <br><br>
                 
                 <label for="jornadaSetmanal">Jornada setmanal</label><br>
-                <input type="number" name="jornadaSetmanal" >
+                <input type="number" name="jornadaSetmanal" required>
                 <br><br>
                 
                 <label for="funcioCarrec">Funció</label><br>
-                <input type="text" name="funcioCarrec" >
+                <input type="text" name="funcioCarrec" required>
+
+                <p style="text-indent: 100%;white-space: nowrap;overflow:hidden;">
+                texto oculto
+                </p>
+
             </div>
+            <button type="submit" class="btn btn-success botonEnviar" names="enviar" value="enviar">Insertar empleat</button>
         </div>
-        <button type="submit" class="btn btn-success botonEnviar" names="enviar" value="enviar">Insertar empleat</button>
     </div>
         <br>
         </form>
         </div>
-
-</body>
-</html>
