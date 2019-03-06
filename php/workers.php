2dd560786970d849a62e3732d3d44423edf9c751
Ralmiron <raquel.a.martinez@hotmail.com>
2019-03-06 15:41:21 +0100

    Revert "include workers"
    This reverts commit f0ece7ac620b0a611bdc62586a233e367e27bcac.

--- php/workers.php
+++ php/workers.php
@@ -1,6 +1,5 @@
 <?php
-    include('php/connect.php');
-    include('php/updateWorkers.php');
+    include('php/connect.php');    
 
     if ($conexion) {
         $db = $conexion->select_db(DB_DATABASE);
