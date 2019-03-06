b3edf8980946887b5869c0e2ae7736d4160789d4
Estebandl <44871992+Estebandl@users.noreply.github.com>
2019-03-06 14:35:54 +0100

    intentando integrar el header

--- php/webelements/header.php
+++ php/webelements/header.php
@@ -1,25 +1,22 @@
-<link rel="icon" type="image/png" href="img/icons/favicon.png"/>
-<link rel="stylesheet" type="text/css" href="css/main.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+<?php 
+  session_start(); 
+  $usuari = $_SESSION['usuari'];
+?>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand">Benvingut/da</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="menu.php">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="consultWorkers.php">Consulta</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="insertWorkers.php">Insertar</a>
-      </li>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+
+<div id = "header">
+
+    <ul class="nav">
+    <li><img class="imagen" src="img/logo.png"/ style="">
+    <ul><br>
+          <li><a class="colores" href="">Home</a></li><br>
+          <li><a class="colores" href="consultWorkers.html">Consulta</a></li>
+        </ul>
+    </li>
+
+      <li><a class="margenes" href="">Benvingut/da, <?php echo $usuari ?></a></li>
+        <div><img class="apagado" src="img/icons/logout.png"/></div>
     </ul>
-  </div>
-</nav>
+
+</div>
